b74dc1dbc6c8223970316209e819fb9fffd510ea
MASHA <mashanikit@gmail.com>
2017-06-28 17:45:09 -0400

    done

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,14 +1,22 @@
 class PokemonController < ApplicationController
   def show
 
-    res = Typhoeus.get("http://pokeapi.co/api/v2/pokemon/pikachu/", followlocation: true)
+    res = Typhoeus.get("http://pokeapi.co/api/v2/pokemon/"+params[:id], followlocation: true)
     body = JSON.parse(res.body)
 
-    render json: {
+
+    res_gif = Typhoeus.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_KEY"]}&q="+params[:id], followlocation: true)
+    body_gif = JSON.parse(res_gif.body)
+
+
+
+    render json:
+    { url: body_gif["data"][0]["embed_url"] ,
       name: body["name"],
       id: body["id"],
       type: body ["types"].map do |type_hash|
         type_hash["type"]["name"]
+
       end
 
     }
