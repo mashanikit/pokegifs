c81dd247b703f5e21d4c88b671739d228e1eda79
MASHA <mashanikit@gmail.com>
2017-06-28 16:36:53 -0400

    renders json info, id, name and type

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,7 +1,21 @@
 class PokemonController < ApplicationController
-  def index
+  def show
+
+    res = Typhoeus.get("http://pokeapi.co/api/v2/pokemon/pikachu/", followlocation: true)
+    body = JSON.parse(res.body)
+
     render json: {
-      "message": "ok"
+      name: body["name"],
+      id: body["id"],
+      types: body ["types"]
+      # body["id"]
+      # body["types"]
     }
+
+
+
+ # should be "pikachu"
+    # body ["id"]
+    # body ["types"]
   end
 end
